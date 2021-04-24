f79fa7239cfa6a9843c084d6092b57e9c4ef6ab3
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-04-24 14:43:40 +0100

    Removed cookie and sttatic middleware

--- server/main/app.js
+++ server/main/app.js
@@ -1,7 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const linkRouter = require('./routes/linkRoutes')
 const authRoutes = require('./routes/authRoutes')
@@ -12,8 +10,6 @@ app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/auth', authRoutes)
 app.use('/', linkRouter)
 
