a4c0ec8e980fa89bd8b89e4642ba7f3b983f9842
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-07 23:39:25 +0100

    Added routes to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const NavbarComponent = (props) => {
+
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <header>
+         Short link
+      </header>
+      <nav>
+        <Link to="/">Home</Link>
+          
+        {!localStorage.getItem('token')  ? 
+          <div>
+            <Link to="/login">Sign in</Link>
+            <Link to="/register">Register</Link>
+          </div> : 
+          <div>
+            <Link to="/dashboard" >Dashboard</Link>
+            <Link to="/logout" >Logout</Link>
+          </div>
+        }
+      </nav>
+    </nav>
   )
 }
 
-export default Navbar
+export default NavbarComponent
