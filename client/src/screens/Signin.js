0d7fc3e2f465937a7437f2f1401e25f28701ad0f
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-07 23:45:33 +0100

    Created dummy signin and signup page

--- client/src/screens/Signin.js
+++ client/src/screens/Signin.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import {useRef} from 'react'
 import { useHistory } from "react-router-dom"
 
 const Signin = () => {
   const history = useHistory()
   localStorage.getItem('token') && history.push('/dashboard')
+  const email = useRef('')
+  const password = useRef('')
+  const handleClick = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
       Signin Page
+      <form action="">
+        {/* Email */}
+        <div>
+          <label htmlFor="email">Email:  </label>
+          <input type="email" ref={email} />
+        </div>
+
+        {/* Password */}
+        <div>
+          <label htmlFor="password">Password:  </label>
+          <input type="password" ref={password} />
+        </div>
+
+        {/* button */}
+        <button onClick={handleClick}>Login</button>
+      </form>
     </div>
   )
 }
--- client/src/screens/Signup.js
+++ client/src/screens/Signup.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import {useRef} from 'react'
 import { useHistory } from "react-router-dom"
 
 const Signup = () => {
   const history = useHistory()
   localStorage.getItem('token') && history.push('/dashboard')
+  const email = useRef('')
+  const password = useRef('')
+  const handleClick = (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
       Signup Page
+      <form action="">
+        {/* Email */}
+        <div>
+          <label htmlFor="email">Email:  </label>
+          <input type="email" ref={email} />
+        </div>
+
+        {/* Password */}
+        <div>
+          <label htmlFor="password">Password:  </label>
+          <input type="password" ref={password} />
+        </div>
+
+        {/* button */}
+        <button onClick={handleClick}>Register</button>
+      </form>
     </div>
   )
 }
