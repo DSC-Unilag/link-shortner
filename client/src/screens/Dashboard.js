b9e13d291b1aab719d774d8158a4876205b0ad2c
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-08 13:47:38 +0100

    Created dummy data in dashboard

--- client/src/screens/Dashboard.js
+++ client/src/screens/Dashboard.js
@@ -1,22 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from "react-router-dom"
 import Navbar from '../components/Navbar'
 
 const Dashboard = () => {
   const history = useHistory()
   !localStorage.getItem('token') && history.push('/')
-
+  const [clicked, setClicked] = useState(false)
+  const [data, setData] = useState([])
+  useEffect(() => {
+    setData([
+      {
+        id: 1,
+        title: 'App',
+        link: 'https://google.com',
+        shortened_url: 'app/g/'
+      },
+      {
+        id: 2,
+        title: 'App',
+        link: 'https://google.com',
+        shortened_url: 'app/g/'
+      }
+    ])
+    return () => {
+      'cleanup'
+    }
+  }, [])
+  const handleClick = e => {
+    e.preventDefault()
+    console.log(e.target.title.value)
+    console.log(e.target.link.value)
+    if (e.target.name.value && e.target.link.value) {
+      setData([
+        {
+          id: data.length+1,
+          title: e.target.title.value,
+          link: e.target.link.value,
+          shortened_url: '/gg/gg/'
+        },
+        ...data
+      ])
+      setClicked(false)
+    }
+  }
   return (
     <div>
       <Navbar /> <br /><hr />
       Dashboard page <br /> <hr />
-      <button>New URL</button>
-      <form action="">
+      {
+      !clicked ? 
+      <button onClick={() => {setClicked(true)}}>New URL</button> :
+      <form onSubmit={handleClick}>
         <div>
           <label htmlFor="title">Title</label>
+          <input type="text"  name="title" />
         </div>
+
+        <div>
+          <label htmlFor="link">Link</label>
+          <input type="url"  name="link" />
+        </div>
+
+        <button>Create</button>
       </form>
+      
+      } <br /> <hr />
+
+      <table>
+        <thead>
+          <td>Title</td>
+          <td>Link</td>
+          <td>Shortened URL</td>
+          <td>Edit</td>
+          <td>Delete</td>
+        </thead>
+        <tbody>
+          {data.map(ele => {
+            return <Link 
+                      key={ele.id} 
+                      ele={ele}
+                      handleDelete={() => {
+                        setData(data.filter(d => d.id !== ele.id))
+                      }} 
+                      handleEdit ={() =>{
+                        setClicked(true)
+                      }}
+                    />
+          })}
+        </tbody>
+      </table>
     </div>
+  )
+}
+
+const Link = ({ele, handleDelete, handleEdit}) => {
+  return (
+    <tr>
+      <td>{ele.title}</td>
+      <td>{ele.link}</td>
+      <td>{ele.shortened_url}</td>
+      <td><button onClick={handleEdit}>Edit</button></td>
+      <td><button onClick={handleDelete}>Delete</button></td>
+    </tr>
   )
 }
 
